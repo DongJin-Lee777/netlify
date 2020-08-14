718ec22d7655bfe4b58e180abbacb176d34711b4
DongJin-Lee777 <leik24@naver.com>
2020-08-14 23:40:02 +0900

    1

--- js/index.js
+++ js/index.js
@@ -4,4 +4,12 @@ $(function() {
             const target = e.target.id;
             $('#func_contain_'+target).toggleClass('action');
         })
+        
+        
+        $('#footer').css("width", $(window).width()-60+"px");
+
+        $(window).resize(function() {
+            var w = $(window).width();
+            $('#footer').css("width", w-60+"px");
+        })
 })
